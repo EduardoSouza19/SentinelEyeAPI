a7094fc6d3715b01a395d77e4735d538914b99ac
EduardoSouza19 <eduardoaugusto.souza06@gmail.com>
2026-05-27 20:10:10 -0300

    commit final do projeto

--- Services/ImagemService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SentinelEye.Services
-{
-    public class ImagemService
-    {
-    }
-}
--- Services/OcorrenciaService.cs
+++ Services/OcorrenciaService.cs
@@ -1,6 +1,40 @@
-﻿namespace SentinelEye.Services
+﻿using SentinelEye.Models;
+using SentinelEye.Repositories.Interfaces;
+using SentinelEye.Services.Interfaces;
+
+namespace SentinelEye.Services;
+
+public class OcorrenciaService : IOcorrenciaService
 {
-    public class OcorrenciaService
+    private readonly IOcorrenciaRepository _repository;
+
+    public OcorrenciaService(IOcorrenciaRepository repository)
     {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Ocorrencia>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Ocorrencia?> GetByIdAsync(long id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<Ocorrencia> CreateAsync(Ocorrencia ocorrencia)
+    {
+        return await _repository.CreateAsync(ocorrencia);
+    }
+
+    public async Task<bool> UpdateAsync(long id, Ocorrencia ocorrencia)
+    {
+        return await _repository.UpdateAsync(id, ocorrencia);
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        return await _repository.DeleteAsync(id);
     }
 }
